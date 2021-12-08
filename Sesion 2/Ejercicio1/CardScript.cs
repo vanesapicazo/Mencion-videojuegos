b73c70c39f00942fe1081cb4178e643a470b5c17
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2021-12-08 19:02:04 +0100

    Add files via upload

---
+++ Sesion 2/Ejercicio1/CardScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public Sprite imagen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GetComponent<SpriteRenderer>().sprite = imagen;
+
+        transform.position = new Vector3(-1, 3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
