095edf0b063323dc43a1281d53462cf38abef361
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2021-12-14 15:54:08 +0100

    Add files via upload

---
+++ sesion 3/Ejercicio 3/CardScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    SpriteRenderer myRenderer;
+    public Sprite front;
+    public Sprite back;
+
+    bool faceUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("He hecho clic en la carta " + name);
+
+        if (!faceUp)
+        {
+            myRenderer.sprite = front;
+        }
+        else
+        {
+            myRenderer.sprite = back;
+        }
+
+        faceUp = !faceUp;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ sesion 3/Ejercicio 3/GameManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript: MonoBehaviour
+{
+    public GameObject myPrefab;
+    public List<GameObject> cartas = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float posX = -8;
+        float posY = 3;
+
+        GameObject nueva_carta;
+
+        for(int i = 0; i < 10; i++)
+        {
+            nueva_carta = Instantiate(myPrefab, new Vector3(posX, posY, 0), Quaternion.identity);
+            nueva_carta.name = "card" + i;
+            cartas.Add(nueva_carta);
+
+            posX = posX + 3;
+
+            if(i == 4)
+            {
+                posX = -8;
+                posY = -2;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
