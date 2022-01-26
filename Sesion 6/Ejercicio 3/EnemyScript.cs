bf60e23fa907602e3b59be86725aa422963f8cb2
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2022-01-26 20:32:31 +0100

    Commit ejercicio 3

---
+++ Sesion 6/Ejercicio 3/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.8f);
+        MoveEnemy();
+        StartCoroutine(Wait());
+    }
+
+    void MoveEnemy()
+    {
+        transform.Translate(Vector3.right);
+    }
+}
