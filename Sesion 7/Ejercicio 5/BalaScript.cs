707690e0da1004ea756f332b049d2d9877be1a5f
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2022-01-31 17:48:29 +0100

    Commit ejercicio 5

---
+++ Sesion 7/Ejercicio 5/BalaScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaScript : MonoBehaviour
+{
+
+    GameObject spaceShip;
+    SpaceShipScript scriptNave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceShip = GameObject.FindGameObjectWithTag("Nave");
+        scriptNave = spaceShip.GetComponent<SpaceShipScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("He chocado con " + collision.gameObject.name);
+
+        scriptNave.SetDisparar();
+        Destroy(gameObject);
+    }
+}
---
+++ Sesion 7/Ejercicio 5/SpaceShipScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipScript : MonoBehaviour
+{
+    public int force;
+    public int forceTorpedo; 
+    Rigidbody2D myRB;
+    public GameObject torpedo;
+    bool disparo = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        myRB.velocity = transform.right * movement * force;
+
+        float xPos = Mathf.Clamp(myRB.position.x, -6.66f, 6.66f);
+
+        myRB.position = new Vector2(xPos, myRB.position.y);
+
+        if (Input.GetButton("Jump") && disparo)
+        {
+            Vector2 posTorpedo = new Vector2(transform.position.x, transform.position.y + 1.278f);
+            GameObject clone = Instantiate(torpedo, posTorpedo, Quaternion.identity);
+
+            Rigidbody2D cloneRB = clone.GetComponent<Rigidbody2D>();
+
+            Vector2 direccion = new Vector2(0f, 1f);
+
+            cloneRB.velocity = direccion * forceTorpedo;
+
+            disparo = false;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("NAVE: Collision");
+    }
+
+    public void SetDisparar()
+    {
+        disparo = true;
+    }
+}
