b2e18f8a633f74088889f6849241ba39970e880c
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2022-01-31 20:26:34 +0100

    Commit ejercicio 6

---
+++ Sesion 7/Ejercicio 6/BalaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaScript : MonoBehaviour
+{
+
+    GameObject spaceShip;
+    SpaceShipScript scriptNave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceShip = GameObject.FindGameObjectWithTag("Nave");
+        scriptNave = spaceShip.GetComponent<SpaceShipScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        scriptNave.SetDisparar();
+        Destroy(gameObject);
+    }
+}
