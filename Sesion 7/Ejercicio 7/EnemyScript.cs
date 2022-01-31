272c3a2037be8b1982cf17649f089769c43cc2f6
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2022-01-31 22:56:20 +0100

    Commit ejercicio 6

---
+++ Sesion 7/Ejercicio 7/EnemyScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    bool move = true;
+    float sleep = 0.8f;
+    bool jugando = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(sleep);
+
+        if (jugando)
+        {
+            MoveEnemy();
+        }
+
+        StartCoroutine(Wait());
+    }
+
+    private void MoveEnemy()
+    {
+        if (move == true)
+        {
+
+            if(transform.position.x > 5)
+            {
+                
+                move = false;
+                sleep -= 0.05f;
+                transform.Translate(Vector2.down);
+            }
+            else
+            {
+                transform.Translate(Vector2.right);
+            }
+        }
+        else
+        {
+
+            if(transform.position.x < -5)
+            {
+                move = true;
+                sleep -= 0.05f;
+                transform.Translate(Vector2.down);
+            }
+            else
+            {
+                transform.Translate(Vector2.left);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "bala")
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "Nave")
+        {
+            jugando = false;
+        }
+        
+    }
+}
---
+++ Sesion 7/Ejercicio 7/SpaceShipScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipScript : MonoBehaviour
+{
+    public int force;
+    public int forceTorpedo; 
+    Rigidbody2D myRB;
+    public GameObject torpedo;
+    public bool disparo = true;
+
+    bool jugando = true;
+
+    GameObject gameover;
+    GameObject youwin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        gameover = GameObject.FindGameObjectWithTag("gameover");
+        gameover.SetActive(false);
+        youwin = GameObject.FindGameObjectWithTag("win");
+        youwin.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (jugando)
+        {
+            float movement = Input.GetAxis("Horizontal");
+
+            myRB.velocity = transform.right * movement * force;
+
+            float xPos = Mathf.Clamp(myRB.position.x, -6.66f, 6.66f);
+
+            myRB.position = new Vector2(xPos, myRB.position.y);
+
+            if (Input.GetButton("Jump") && disparo)
+            {
+                Vector2 posTorpedo = new Vector2(transform.position.x, transform.position.y + 1.278f);
+                GameObject clone = Instantiate(torpedo, posTorpedo, Quaternion.identity);
+
+                Rigidbody2D cloneRB = clone.GetComponent<Rigidbody2D>();
+
+                Vector2 direccion = new Vector2(0f, 1f);
+
+                cloneRB.velocity = direccion * forceTorpedo;
+
+                disparo = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            end();
+
+            gameover.SetActive(true);
+        }
+    }
+
+    public void SetDisparar()
+    {
+        disparo = true;
+    }
+
+    public void end()
+    {
+        jugando = false;
+        youwin.SetActive(true);
+    }
+}
