e17897e7876e71fb1438dc59ce92b0f9ff952bec
vanesapicazo <75306581+vanesapicazo@users.noreply.github.com>
2022-01-31 17:20:06 +0100

    Commit ejercicio 4

---
+++ Sesion 7/Ejercicio 4/EnemyScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    bool derecha = true;
+    float sleep = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(sleep);
+        MoveEnemy();
+        StartCoroutine(Wait());
+    }
+
+    void MoveEnemy()
+    {
+        if (derecha)
+        {
+
+            if(transform.position.x > 5)
+            {
+                sleep -= 0.05f;
+                derecha = false;
+                transform.Translate(Vector3.down);
+            }
+            else
+            {
+                transform.Translate(Vector3.right);
+            }
+        }
+        else
+        {
+
+            if(transform.position.x < -5)
+            {
+                sleep -= 0.05f;
+                derecha = true;
+                transform.Translate(Vector3.down);
+            }
+            else
+            {
+                transform.Translate(Vector3.left);
+            }
+        }
+
+
+
+    }
+}
